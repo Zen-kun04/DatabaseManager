bdc2d8dff33f211f728f507f96b125d47f945d30
Baguette <veryveroniquevery@gmail.com>
2023-05-30 22:00:21 +0200

    Initial commit

---
+++ src/main/java/com/donbaguette/databasemanager/DatabaseManager.java
@@ -0,0 +1,17 @@
+package com.donbaguette.databasemanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DatabaseManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
